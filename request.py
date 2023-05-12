ce5d35c03b857227355f1c4456895e82138c993d
Alexandre F Silva <alexsilva.ts@gmail.com>
2023-05-12 19:54:39 -0300

    Update request.py

--- request.py
+++ request.py
@@ -4,12 +4,18 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/tipo')
+@app.route('/v1/tipo')
 def home():
     api_url = "https://jsonplaceholder.typicode.com/todos/1"
     response = requests.get(api_url)
     return response.json()
 
+@app.route('/v1/tipo', methods=["POST"])
+def cadastrar():
+    api_url = "https://jsonplaceholder.typicode.com/todos"
+    enviar = {"userId": 1, "title": "Buy milk", "completed": False}
+    response = requests.post(api_url, json=enviar)
+    return response.json()
 
 if __name__ == '__main__':
     app.run()
